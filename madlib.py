2133ac089593a72ceee7037fd5366d5c0639f7a1
aligulfaamstudent <90401861+aligulfaamstudent@users.noreply.github.com>
2022-08-24 08:04:35 -0400

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,9 @@
+noun = input("Enter A noun, Then press Enter: ")
+
+color = input("Enter a color Then Press Enter: ") 
+
+verb = input("Enter a Verb (present tense), Then Press Enter: ")
+
+madlibsstory = ("Today, every student has a computer small enough to fit into his " + noun + ". and it can be " + color + ". Computers can add, mulitply, divide, and " + verb + ".")
+
+print(madlibsstory)
